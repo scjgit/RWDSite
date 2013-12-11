f68e3d6e2bb5f5d05993e8bed98190998e7f3c82
scj <sallapcjoseph@gmail.com>
2013-12-11 14:44:45 +0530

    Initial Commit

---
+++ WebContent/js/custom/my-site.js
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+function goToHomePage(){
+	window.location.replace("/RWDSite/home.html");
+}
+
+function goToIndexPage(){
+	window.location.replace("/RWDSite/index.html");
+}
+
+function goToMyprofilePage(){
+	window.location.replace("/RWDSite/myprofile.html");
+}
+
+function goToProductHomePage(){
+	window.location.replace("/RWDSite/producthome.html");
+}
